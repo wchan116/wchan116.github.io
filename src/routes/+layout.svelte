8b5017c571d2311ff1652de7e4c8b82afc353da5
wchan116 <wmchan622+github116@gmail.com>
2025-12-08 23:10:32 +1100

    adjust navbar spacing

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,14 +12,16 @@
 </svelte:head>
 
 <nav class="navbar bg-neutral shadow-sm">
-	<div class="flex-1">
-		<a class="btn btn-ghost text-xl" href={resolve('/')}>wchan</a>
-	</div>
-	<div class="flex-none">
-		<ul class="menu menu-horizontal px-1">
-			<li><a class="link link-hover" href={resolve('/about')}>About</a></li>
-			<li><a class="link link-hover" href={resolve('/blog')}>Blog</a></li>
-		</ul>
+	<div class="container flex max-w-7xl m-auto justify-between md:px-16">
+		<div class="flex-1">
+			<a class="btn btn-ghost text-xl" href={resolve('/')}>wchan</a>
+		</div>
+		<div class="flex-none">
+			<ul class="menu menu-horizontal px-1">
+				<li><a class="link link-hover" href={resolve('/about')}>About</a></li>
+				<li><a class="link link-hover" href={resolve('/blog')}>Blog</a></li>
+			</ul>
+		</div>
 	</div>
 </nav>
 
